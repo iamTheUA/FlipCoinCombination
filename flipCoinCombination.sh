d75c597b7584ba30f247ed760891ff1ddae64be8
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-07-08 16:30:20 +0530

    Added Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -16,17 +16,17 @@ read -p "Enter the Number of Coin Flip: " numberOfCoinFlip
 #Store herd count and Tail count singletFlip Dictionary
 for(( count=0; count<$numberOfCoinFlip; count++ ))
 do
-   Flip
+	Flip
 
 #if flip is 0 then it is heads else it is tails
-   if [ $flip -eq 0 ]
-   then
+if [ $flip -eq 0 ]
+then
 	headCount=$((headCount+1))
 	singletFlip[HEAD]=$headCount
-   else
+else
 	tailCount=$((tailCount+1))
 	singletFlip[TAIL]=$tailCount
-   fi
+fi
 done
 
 singletHeadPercentage=$((($headCount*100)/$numberOfCoinFlip))
@@ -37,3 +37,49 @@ echo "Singlet Head percentage: " $singletHeadPercentage
 echo "Head Count: "$headCount
 echo "Singlet Tail percentage: " $singletTailPercentage
 echo "Tail Count: "$tailCount
+
+#For Doublet creating Dictionary
+declare -A doubletFlip
+
+#fuction for adding to dictionary
+function doublet()
+{
+	for(( count=0; count<$numberOfCoinFlip; count++ ))
+	do
+		for(( countCoin=0; countCoin<2; countCoin++ ))
+		do
+        		Flip
+        		if [ $flip -eq 0 ]
+        		then
+        			HOrt+=H
+        		else
+        			HOrT+=T
+        		fi
+		done
+		doubletFlip[$HOrT]=$((doubletFlip[$HOrT]+1))
+		HOrT=""
+	done
+
+#function for calculating Doublet percentage
+function doubletPercentage()
+{
+	for i in "${!doubletFlip[@]}"
+   	do
+   		doubletFlip[$i]=$(((${doubletFlip[$i]}*100)/$numberOfCoinFlip))
+   	done
+
+}
+
+doublet
+for i in "${!doubletFlip[@]}"
+do
+	echo "Counts:-"
+	echo "$i: " ${doubletFlip[$i]}
+done
+
+doubletPercentage
+for i in "${!doubletFlip[@]}"
+do
+	echo "Percentage:-"
+        echo "$i: " ${doubletFlip[$i]}
+done
